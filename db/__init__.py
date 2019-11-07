b140d59ec2275dc97eebdb94e139efce1fed5496
Tuomo <tuokri@tuta.io>
2019-11-07 19:27:43 +0200

    Expose init_db

--- db/__init__.py
+++ db/__init__.py
@@ -1,9 +1,11 @@
 from .db import get_log_cache
+from .db import init_db
 from .db import insert_log_cache
 from .db import update_log_cache
 
 __all__ = [
     "get_log_cache",
+    "init_db",
     "insert_log_cache",
     "update_log_cache",
 ]
