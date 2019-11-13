de87369fe0b045302831b0203acd161e1aea6b8b
Tuomo <tuokri@tuta.io>
2019-11-14 01:23:49 +0200

    Expose collectors module

--- rs2wat/__init__.py
+++ rs2wat/__init__.py
@@ -1,10 +1,11 @@
 from .collectors import FTPCollector
 from .db import db
 
-__version__ = "0.1.11"
+__version__ = "0.1.12"
 
 __all__ = [
     "__version__",
     "FTPCollector",
     "db",
+    "collectors",
 ]
