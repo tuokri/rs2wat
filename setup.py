ebc06c59fe522b2fdf6b952951ed922b710b2104
Tuomo <tuokri@tuta.io>
2019-11-13 23:35:57 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="rs2wat",
-    version="0.1.7",
+    version="0.1.8",
     packages=setuptools.find_packages(),
     url="https://github.com/tuokri/rs2wat",
     author="tuokri",
