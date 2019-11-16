8880865287ab346af9edef495a7d33855bf74e6c
Tuomo <tuokri@tuta.io>
2019-11-16 18:23:31 +0200

    Fix update query

--- rs2wat/__init__.py
+++ rs2wat/__init__.py
@@ -1,7 +1,7 @@
 from .collectors import FTPCollector
 from .db import db
 
-__version__ = "0.2.14"
+__version__ = "0.2.15"
 
 __all__ = [
     "__version__",
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="rs2wat",
-    version="0.2.14",
+    version="0.2.15",
     packages=setuptools.find_packages(),
     url="https://github.com/tuokri/rs2wat",
     author="tuokri",
